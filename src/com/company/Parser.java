015732e64ada7da5902feab948910fa155308b4e
Rost <rlbozhko@gmail.com>
2015-12-22 15:07:12 +0200

    Initial commit

---
+++ src/com/company/Parser.java
@@ -0,0 +1,85 @@
+package com.company;
+
+
+import org.htmlcleaner.*;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+
+public class Parser {
+
+    public static void main(String[] args) {
+        try {
+
+            TagNode tagNode = new HtmlCleaner().clean(
+                    "<div><table><td id='1234 foo 5678'>Hello</td>");
+            org.w3c.dom.Document doc = new DomSerializer(
+                    new CleanerProperties()).createDOM(tagNode);
+
+
+            XPath xpath = XPathFactory.newInstance().newXPath();
+            String str = (String) xpath.evaluate("//div//td[contains(@id, 'foo')]/text()",
+                    doc, XPathConstants.STRING);
+            System.out.println(str);
+
+    /*        HtmlCleaner cleaner = new HtmlCleaner();
+            TagNode html = cleaner.clean(new File("source.html"));
+
+            // Полный XPath к элементу title, корневой элемент /html не указывается
+            // так как поиск в нем производится
+            String title = html.evaluateXPath("/head/title/text()")[0].toString().trim();
+            System.out.println("title\t: " + title);
+
+            // Находим элементы <div> где аттрибут class="menu" в любом месте
+            // в котором есть элемент <h1>. Таких элементов только один, берем первый
+            String header = html.evaluateXPath("//div[@class='menu']/h1/text()")[0].toString().trim();
+            System.out.println("header\t: " + header);
+
+            // Находим элементы <div> где аттрибут class="menu" в любом месте
+            // в котором есть элемент <a>. Таких элементов много, обходим все.
+            Object[] tags = html.evaluateXPath("//div[@class='menu']/a");
+            for (Object tag : tags) {
+                TagNode aTag = (TagNode) tag;
+                String href = aTag.getAttributeByName("href").trim();
+                String content = aTag.getText().toString().trim();
+                System.out.println("link\t: " + content + "[" + href + "]");
+            }
+
+            // Достаем текст элемента <div class="content">
+            // TagNode.getText() не подходит так как он достает текст всех дочерних
+            // элементов
+            TagNode contentNode = (TagNode) html.evaluateXPath("//div[@class='content']")[0];
+            String contentText = getContent(contentNode).trim();
+            System.out.println("content\t: " + contentText);
+
+            // Достаем все элементы <li>
+            tags = html.evaluateXPath("//div[@class='content']/ul/li/text()");
+            for (Object tag : tags) {
+                System.out.println("color\t: " + tag.toString().trim());
+            }
+
+            // Достаем ссылку на картинку
+            String imageHref = html.evaluateXPath("//div[@class='content']/img[@class='preview']/@src")[0].toString().trim();
+            System.out.println("image\t: " + imageHref);
+
+            // Весь текст включая, текст дочерних элементов тэга <div class="bottom">
+            TagNode bottomNode = (TagNode) html.evaluateXPath("//div[@class='bottom']")[0];
+            String bottom = bottomNode.getText().toString().trim();
+            System.out.println("bottom\t: " + bottom);
+*/
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getContent(TagNode node) {
+        StringBuilder result = new StringBuilder();
+        for (Object item : node.getChildren()) {
+            if (item instanceof ContentNode) {
+                result.append(((ContentNode) item).getContent());
+            }
+        }
+        return result.toString();
+    }
+}
