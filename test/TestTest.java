d72d6e85c8b2485d2536f15e4960da7e28f9ae47
Rost <rlbozhko@gmail.com>
2015-12-26 22:21:22 +0200

    regular expression

--- test/TestTest.java
+++ test/TestTest.java
@@ -4,12 +4,12 @@ public class TestTest {
 
     @org.junit.Test
     public void testRegular() {
-          final Pattern ROZETKA_CATEGORY =  Pattern.compile(".*/c[0-9]*/filter/|.*/c[0-9]*/");
+          final Pattern ROZETKA_CATEGORY =  Pattern.compile(".*/c\\d*(/filter/|/)");
         //Pattern.compile(".*/c[0-9]*/filter/|.*/c[0-9]*/");
-        //Pattern.compile(".*/c[0-9]*(/filter/)|(/)");
+        //Pattern.compile(".*/c[0-9]*((/filter/)|(/))");
         // http://rozetka.com.ua/mobile-phones/c80003/filter/
         // "http://rozetka.com.ua/mobile-phones/apple/c80003/v069/"
-        String href = "http://rozetka.com.ua/mobile-phones/c80003/filter/";
+        String href = "http://rozetka.com.ua/mobile-phones/c80003/";
         if (ROZETKA_CATEGORY.matcher(href).matches()) {
             System.out.println("YES");
 
